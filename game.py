01e1caf6f13d6370be55965efdfbdaaa227629f2
Wal0fsky <namakemono1810@gmail.com>
2026-06-11 16:50:36 +0200

    rendered image and hitbox, need to adjust rotation function for the hitbox

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 from logic import move_player, rotate_player
 
 pygame.init() # source: https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1
@@ -9,8 +10,13 @@ dt = 0
 
 class Player:
     def __init__(self):
-        self.angle = 0
+        self.angle = 0 # so it is vertical
         self.pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+        self.original_image = pygame.image.load(os.path.join("player.png")).convert_alpha()
+        self.original_image = pygame.transform.scale_by(self.original_image, 0.25)
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=(self.pos))
+        self.hitbox = self.image.get_rect(center=(self.pos))
 
 player = Player()
 
@@ -25,7 +31,10 @@ while running: # game loop cycle
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    pygame.draw.circle(screen, "red", player.pos, 40) # GENERATE IMAGE HERE
+    screen.blit(player.image, player.rect)
+    player.rect = player.image.get_rect(center=(player.pos))
+    player.hitbox.center = player.pos
+    pygame.draw.rect(screen, (255, 0, 0), player.hitbox, 2) # for debugging
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -33,9 +42,15 @@ while running: # game loop cycle
     if keys[pygame.K_s]:
         move_player(False, player, dt)
     if keys[pygame.K_a]:
-        angle = rotate_player(True, player, dt)
+        if keys[pygame.K_s]:
+            rotate_player(False, player, dt)
+        else:
+            rotate_player(True, player, dt)
     if keys[pygame.K_d]:
-        angle = rotate_player(False, player, dt)
+        if keys[pygame.K_s]:
+            rotate_player(True, player, dt)
+        else:
+            rotate_player(False, player, dt)
 
     pygame.display.flip() # pygame generates stuff on a hidden layer and then swaps it to the layer we see so it avoids screen flickering. This allows us to see the stuff after a frame
 
--- logic.py
+++ logic.py
@@ -12,7 +12,12 @@ def move_player(isForward, player, dt):
 def rotate_player(isLeft, player, dt):
     if isLeft: # vertical screen is inverted... why bruv
         player.angle -= dt * 120 
+        player.image = pygame.transform.rotate(player.original_image, -1 * (player.angle))
+        player.hitbox = pygame.transform.rotate(player.hitbox, -1 * (player.angle))
+        
     else:
         player.angle += dt * 120
+        player.image = pygame.transform.rotate(player.original_image, -1 * (player.angle))
+        player.hitbox = pygame.transform.rotate(player.hitbox, -1 * (player.angle))
 
     # transform the figure once we have one
